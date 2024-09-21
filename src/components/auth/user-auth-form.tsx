c0a345412d95e7af3e737abe5a399f08ca117e9a
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-09-22 00:27:19 +0800

    Added Auth

---
+++ src/components/auth/user-auth-form.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import * as React from "react";
+
+import { useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Alert, AlertDescription } from "@/components/ui/alert";
+import { axiosInstance } from "@/lib/axios";
+import { toast } from "../ui/use-toast";
+
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const router = useRouter();
+
+  const [email, setEmail] = React.useState<string>("test@gmail.com");
+  const [password, setPassowrd] = React.useState<string>("123");
+
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
+  async function onSubmit(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const res = await axiosInstance.post("/auth/login", {
+        email,
+        password,
+      });
+
+      const data = res.data as {
+        access_token: string;
+      };
+
+      if (!data.access_token) {
+        throw new Error("Invalid access token");
+      }
+
+      localStorage.setItem("access_token", data.access_token);
+
+      router.replace("/dashboard");
+    } catch (e) {
+      toast({
+        title: "Error",
+        variant: "destructive",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className={cn("grid gap-6", className)} {...props}>
+      <form onSubmit={onSubmit}>
+        <div className="grid gap-2">
+          <Alert variant="default">
+            <AlertDescription className="text-muted-foreground">
+              Demo credentials provided below
+            </AlertDescription>
+          </Alert>
+
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Password
+            </Label>
+            <Input
+              id="password"
+              placeholder="Password"
+              type="password"
+              autoCapitalize="none"
+              autoComplete="password"
+              autoCorrect="off"
+              disabled={isLoading}
+              value={password}
+              onChange={(e) => setPassowrd(e.target.value)}
+            />
+          </div>
+          <Button disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign In with Email
+          </Button>
+        </div>
+      </form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <Button
+        variant="outline"
+        type="button"
+        className="cursor-not-allowed"
+        disabled={true && isLoading}
+      >
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.gitHub className="mr-2 h-4 w-4" />
+        )}{" "}
+        GitHub
+      </Button>
+    </div>
+  );
+}
--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -12,7 +12,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built with ❤️ in Aptos Winter School S2
+            Built with ❤️ in ETH Singapore 2024
           </p>
         </div>
         <ModeToggle />
